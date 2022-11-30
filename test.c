e209b8de0357ba3679ac907a7444ce5faf5befb4
Hassan Ali Qadir <70215127+Liquidburn1@users.noreply.github.com>
2022-11-30 01:02:37 -0600

    Update test.c

--- test.c
+++ test.c
@@ -25,7 +25,7 @@ typedef struct
     int pas_inside; //The passenger which is inside right now
     int num_req; //number if requests from the passengers
     enum{y=1,n=0} c;
-    enum{y=1,n=0} c2;
+    enum{y1=1,n1=0} c2;
 
 } ele;
 
@@ -36,7 +36,7 @@ ele elevat[ELEVATORS]; //this will crreate the array of struct
 
 
 // Putting the elevators into the array
-void sch_initialzer
+void sch_initialzer()
 {
     int i =0;
 
@@ -47,18 +47,18 @@ void sch_initialzer
         elevat[i].direction=-1;
         elevat[i].pas_inside = -1;
         elevat[i].c = no;
-        elevat[i].c2 = no;
+        elevat[i].c2 = no1;
         elevat[i].type = elevator_came;
         elevat[i].next_one = 0;
-        elevat[i].num_requests = 0;
+        elevat[i].num_req = 0;
         elevat[i].amount=0;
-        pthread_mutex_init(&e[j].l,0);
-        pthread_cond_init(&e[j].pentered, 0);
-        pthread_cond_init(&e[j].pexit, 0);
+        pthread_mutex_init(&elevat[i].l,0);
+        pthread_cond_init(&elevat[i].pentered, 0);
+        pthread_cond_init(&elevat[i].pexit, 0);
         int k=0;
         while(k<=PASSENGERS-1)
         {
-            elevat[i].floor_to_go[i] =-1
+            elevat[i].floor_to_go[i] =-1;
             elevat[i].requesting[k] = -1;
 
             k=k+1;
@@ -90,33 +90,33 @@ void sch_initialzer
 
     }
 
-    int tempfunc(int elevtor)
+    int tempfunc(int elevtor_on)
     {
-        ele *temp = &e[elevator_on];
+        ele *temp = &elevat[elevator_on];
         int i=0;
         while(temp->requesting[temp->next_one]==-1)
         {
             temp->next_one=temp->next_one+1;
-            if(temp->next_one>=PASSENGERS){_e->next_one=0;}
+            if(temp->next_one>=PASSENGERS){temp->next_one=0;}
             if(i+1>PASSENGERS){return 0;}
 
 
 
         }
-        return temp->requesting[temp->next_one++]
+        return temp->requesting[temp->next_one++];
 
     }
     
     //getting the next request from the passengers
     int get_next_req(int elevator_on)
     {
-        ele *temp = &e[elevator_on];
+        ele *temp = &elevat[elevator_on];
         if(temp->next_one>=PASSENGERS){temp->next_one=0;}
-        tempfunc(elevator);
+        tempfunc(elevator_on);
 
     }
 
-    void scheduler_init
+    void scheduler_init()
     {
         sch_initialzer();
         int i=0;
@@ -152,15 +152,15 @@ void sch_initialzer
     {
 
         int temp=assignmenting(from_floor,passenger);
-        elevat[temp].num_requests=elevat[temp].num_requests+1;
+        elevat[temp].num_req=elevat[temp].num_req+1;
         
         //if the passenger is at the floor it should waut for the passenger to get in
         int idle=1;
         while(idle==1)
         {
-            pthread_mutex_l(&elevat[temp].l);
+            pthread_mutex_lock(&elevat[temp].l);
             ele *tem = &elevat[temp];
-            if(tem->from_floor==0)
+            if(tem->this_floor==0)
             {
                tem->c=yes; 
                 pthread_cond_signal(&tem->pentered);
@@ -170,14 +170,14 @@ void sch_initialzer
             else if(tem->type==ele_open && tem->amount==0 && tem->this_floor==from_floor)
             {
                 tem->pas_inside=passenger;
-                tem->floor_to_go[_e->pas_inside]=to_floor;
+                tem->floor_to_go[tem->pas_inside]=to_floor;
                 tem->amount=tem->amount+1;
-                enter(passenger,temp)
+                enter(passenger,temp);
                 idle=-1;
-                tem->c=yes;
+                tem->c=y;
                 pthread_cond_signal(&tem->pentered);
             }
-            pthread_mutex_unl(&elevat[temp].l);
+            pthread_mutex_unlock(&elevat[temp].l);
 
 
         } 
@@ -187,22 +187,22 @@ void sch_initialzer
 
         while(inside_it==1)
         {
-            pthread_mutex_l(&elevat[temp].l);
-            ele *tem=&elevant[temp];
+            pthread_mutex_lock(&elevat[temp].l);
+            ele *tem=&elevat[temp];
             //if its at the desired floor 
             if(tem->type==ele_open && tem->this_floor==to_floor)
             {
             exit(passenger,temp);
             tem->amount=tem->amount-1;
-            tem->num_requests=tem->num_requests-1;
+            tem->num_req=tem->num_req-1;
             tem->pas_inside=-1;
-            remove_request(passenger);
+            remove_it(passenger);
             inside_it=-1;
             }
         
-        pthread_mutex_unl(&elevat[temp].l);
+        pthread_mutex_unlock(&elevat[temp].l);
 
-        tem->c2=yes;
+        tem->c2=y1;
         pthread_cond_signal(&elevat[temp].pexit);
         }
 
@@ -211,31 +211,31 @@ void sch_initialzer
 
     void elevator_ready(int elevator, int at_floor, void(*move_direction)(int, int), void(*door_open)(int), void(*door_close)(int))
     {
-        if(elevat[elevator].num_requests==0)
+        if(elevat[elevator].num_req==0)
         {
             return;
         }
 
         ele *temp = &elevat[elevator];
-        pthread_mutex_l(&temp->l);
+        pthread_mutex_lock(&temp->l);
         
         //checks for the passenger and weather the pasenger is in or not
         if(temp->type==ele_open)
         {
 
-            if (temp->amount == 0 && temp->num_requests > 0) {
-            temp->c = no;
+            if (temp->amount == 0 && temp->num_req > 0) {
+            temp->c = n;
             
             //if passenger is at the floor
-            while (checker(elevator, at_floor)==1 && temp->c == no && temp->num_requests > 0)
+            while (checker(elevator, at_floor)==1 && temp->c == n && temp->num_req > 0)
                 pthread_cond_wait(&temp->pentered, &temp->l);
         }
 
         //checking if there is a passenger inside we are using -1 since pas inside holds the passenger num
             else if(temp->floor_to_go[temp->pas_inside]==at_floor && temp->pas_inside!=-1) 
             {
-                temp->c2=no;
-                while(temp->c2==no)
+                temp->c2=n1;
+                while(temp->c2==n1)
                 {
                     pthread_cond_wait(&temp->pexit,&temp->l);
 
@@ -265,13 +265,13 @@ void sch_initialzer
 
             if(temp->amount!=0)
             {
-                int to_floor=temp->floor_to_go[temp->pas_inide];
+                int to_floor=temp->floor_to_go[temp->pas_inside];
                 temp->direction=to_floor-at_floor;
                 
 
             }
 
-            else if(temp->num_requests>0)
+            else if(temp->num_req>0)
             {
                 int to_floor = get_next_req(elevator);
                 temp->direction=to_floor-at_floor;
@@ -297,36 +297,6 @@ void sch_initialzer
 
          
         }
-        pthread_mutex_unl(&temp->l);
+        pthread_mutex_unlock(&temp->l);
     } 
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
